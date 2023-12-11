0df7f18efad4d7a696b9ee5d69449dd0e9241dad
lukasilverio94 <lukasilverio94@gmail.com>
2023-12-11 22:07:29 +0100

    first commit

---
+++ src/TodoList.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { TodoItem } from "./TodoItem";
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No Todos to display"}
+      {todos.map((todo) => {
+        return (
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        );
+        // id={todo.id}
+        // completed={todo.completed}
+        // title={todo.title}
+        // instead pass like this , pass all the props
+      })}
+    </ul>
+  );
+}
